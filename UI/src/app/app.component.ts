fe3682f0ca4d57189b35f5c73aeeddaaae90e4a5
Herlich <herlich.gonzalez@rocketmatter.com>
2022-11-11 10:05:31 -0600

    updating UI project

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { ngbCarouselTransitionOut } from '@ng-bootstrap/ng-bootstrap/carousel/carousel-transition';
-import { FlashMessagesService } from 'angular2-flash-messages';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +7,4 @@ import { FlashMessagesService } from 'angular2-flash-messages';
 })
 export class AppComponent {
   title = 'UI';
-
-  constructor(private _flashMessagesService: FlashMessagesService) {}
-
-  ngOnInit() {
-    // 1st parameter is a flash message text
-    // 2nd parameter is optional. You can pass object with options.
-    //this._flashMessagesService.show('We are in about component!', { cssClass: 'alert-success', timeout: 1000 });
 }
-
-}
-
-
-
--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FlashMessagesModule } from 'angular2-flash-messages';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -15,9 +14,7 @@ import { FlashMessagesModule } from 'angular2-flash-messages';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    FormsModule,
-    FontAwesomeModule,
-    FlashMessagesModule.forRoot(),
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
