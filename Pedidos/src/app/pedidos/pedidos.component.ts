e0395eb2586a43a1b84b72419c31cedee0340476
Herlich <herlich.gonzalez@rocketmatter.com>
2022-11-14 16:06:37 -0600

    salvando cambios

--- Pedidos/src/app/pedidos/pedidos.component.ts
+++ Pedidos/src/app/pedidos/pedidos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, NgForm } from '@angular/forms';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { LoginModalComponent } from '../login-modal/login-modal.component';
@@ -11,9 +11,10 @@ import { ServiciosService } from '../servicios.service';
 })
 export class PedidosComponent implements OnInit {
 
+  @ViewChild('f') forma: NgForm | undefined;
   pedido: FormGroup = new FormGroup({});;
-
   autenticado: boolean = false;
+
   /**
   * observable to refresh the data when the modal updates.
   */
@@ -28,20 +29,24 @@ export class PedidosComponent implements OnInit {
 
   ngOnInit(): void {
     //this.openDialog();
-    
   }
 
   openDialog() {
     this.matDialog.open(LoginModalComponent, { disableClose: true });
   }
 
-  onSubmit(f:NgForm) { 
-    console.log(f);
+  onSubmit() {
+    if (this.forma?.control.status === "VALID") {
+
+    } else {
+      alert('No se han ingresado todos los campos');
+    }
+    console.log(this.forma);
   }
 
-  onLogout(){
+  onLogout() {
     this.autenticado = false;
-    this.openDialog();    
+    this.openDialog();
   }
 
 }
