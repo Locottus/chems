2545159300bea692a2fba5da6f05f9eb95267d16
Herlich Gonzalez <herlich.gonzalez@mangopractice.com>
2023-05-30 19:37:57 -0600

    added download link

--- Pedidos/src/app/pedidos/componentes/mostrar-pedido/mostrar-pedido.component.ts
+++ Pedidos/src/app/pedidos/componentes/mostrar-pedido/mostrar-pedido.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-mostrar-pedido',
@@ -10,15 +11,21 @@ export class MostrarPedidoComponent implements OnInit {
 
   pedido: any;
   lista: Array<string> = [];
-
+  fileUrl: any;
+  fileReady: boolean = false;
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<MostrarPedidoComponent>,
+    private sanitizer: DomSanitizer
   ) { }
 
   ngOnInit() {
     this.pedido = this.data.pedido;
     this.lista = this.pedido.extendedProps.detalle.toString().split('\n');
+
+    this.saveFile();
+    this.fileReady = true;
+
   }
 
   cerrarPedido() {
@@ -28,6 +35,19 @@ export class MostrarPedidoComponent implements OnInit {
 
   imprimir() {
     window.print();
+  }
+
+  saveFile() {
+    let fix: any = "DETALLE DE PEDIDO\n";
+    this.lista.forEach(e => {
+      if (e.length > 0) {
+        fix = fix + e + '\n';
+      }
+    })
+    const data: any = fix;
+    const blob = new Blob([data], { type: 'application/octet-stream' });
+    this.fileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
+
   }
 
 }
