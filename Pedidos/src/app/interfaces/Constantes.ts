741f64d085c3930605f0407fa1c5e71046a1fd29
Herlich Gonzalez <herlich.gonzalez@mangopractice.com>
2023-06-13 14:37:08 -0600

    changed to prod api

--- Pedidos/src/app/interfaces/Constantes.ts
+++ Pedidos/src/app/interfaces/Constantes.ts
@@ -1,4 +1,4 @@
 export class Constantes {
-    public static backend: string = "http://localhost:3000/api/";
-    //public static backend: string = "https://pedidos.especiaschemita.com/api/";   
+    //public static backend: string = "http://localhost:3000/api/";
+    public static backend: string = "https://pedidos.especiaschemita.com/api/";   
 }
